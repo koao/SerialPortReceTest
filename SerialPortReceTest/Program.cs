5c6be32aaac8c9e0219f49b76568052940c31f67
koao@home <koao8221@gmail.com>
2018-02-05 10:05:04 +0900

    処理記述

--- SerialPortReceTest/Program.cs
+++ SerialPortReceTest/Program.cs
@@ -1,16 +1,52 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.IO.Ports;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace SerialPortReceTest
 {
     class Program
     {
+
+        static SerialPort SP1 = new SerialPort();
+        static SerialPort SP2 = new SerialPort();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Receive test");
+
+            Console.Write("SerialPort1 PortName:");
+            SP1.PortName = Console.ReadLine();
+
+            Console.Write("SerialPort2 PortName:");
+            SP2.PortName = Console.ReadLine();
+
+            SP1.DataReceived += SP1_DataReceived;
+            SP2.DataReceived += SP2_DataReceived;
+
+            SP1.Open();
+            SP2.Open();
+
+            Console.WriteLine("Receive wait");
+
+            Console.ReadKey(true);
         }
+
+        private static void SP1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] data = new byte[SP1.BytesToRead];
+            SP1.Read(data, 0, SP1.BytesToRead);
+            Console.WriteLine("Receive:" + Encoding.GetEncoding(932).GetString(data));
+        }
+
+        private static void SP2_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(100000);
+
+            byte[] data = new byte[SP2.BytesToRead];
+            SP2.Read(data, 0, SP2.BytesToRead);
+            Console.WriteLine("Receive:" + Encoding.GetEncoding(932).GetString(data));
+        }
+
     }
 }
---
+++ SerialPortSendTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SerialPortSendTest
+{
+    class Program
+    {
+
+        static SerialPort SP1 = new SerialPort();
+        static SerialPort SP2 = new SerialPort();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send test");
+
+            Console.Write("SerialPort1 PortName:");
+            SP1.PortName = Console.ReadLine();
+
+            Console.Write("SerialPort2 PortName:");
+            SP2.PortName = Console.ReadLine();
+
+            SP1.Open();
+            SP2.Open();
+
+            Console.WriteLine("Send start");
+
+            Send();
+
+            Console.ReadKey(true);
+        }
+
+        static void Send()
+        {
+            Task task = Task.Factory.StartNew(() => {
+                while(true)
+                {
+                    SendMsg(SP1, "SerialPort1");
+                    Thread.Sleep(500);
+                    SendMsg(SP2, "SerialPort2");
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+        static void SendMsg(SerialPort serialPort,string str)
+        {
+            byte[] data = Encoding.GetEncoding(932).GetBytes(str);
+            serialPort.Write(data, 0, data.Length);
+            Console.WriteLine("Send:" + str);
+        }
+    }
+}
